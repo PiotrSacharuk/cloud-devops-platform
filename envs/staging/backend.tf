4409223e622ecccf1746cb761e8ec9453cd11c08
Piotr Sacharuk <piotr.sacharuk@gmail.com>
2026-02-18 16:02:50 +0100

    Add staging environment

--- envs/staging/backend.tf
+++ envs/staging/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "cloud-devops-platform-free-tier"
-    key            = "cloud-devops-platform/dev/terraform.tfstate"
+    key            = "cloud-devops-platform/staging/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
