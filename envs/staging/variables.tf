397852c9dd289ca2b45e8fe1fdc37336918e3eab
Piotr Sacharuk <piotr.sacharuk@gmail.com>
2026-02-18 23:34:25 +0100

    Make staging SSM-only (disable SSH key pair and SG ingress 22)

--- envs/staging/variables.tf
+++ envs/staging/variables.tf
@@ -9,12 +9,6 @@ variable "ami_id" {}
 
 variable "instance_type" {}
 
-variable "key_name" {}
-
-variable "public_key_path" {}
-
-variable "enable_ssh" {}
-
 variable "environment" {
   type = string
 }
