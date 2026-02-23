e1ca6eb6aaaf2e05f633c432fe1eebe593c838a9
Piotr Sacharuk <piotr.sacharuk@gmail.com>
2026-02-23 15:32:34 +0100

    Private network and DIY NAT instance

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -114,3 +114,129 @@ resource "aws_security_group" "web" {
     }
   )
 }
+
+###########################
+# Private Subnet & Routing
+###########################
+resource "aws_subnet" "private" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.private_cidr
+  map_public_ip_on_launch = false
+  availability_zone       = var.availability_zone
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.environment}-private-subnet"
+    }
+  )
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.environment}-private-rt"
+    }
+  )
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
+
+###########################
+# DIY NAT Instance
+###########################
+data "aws_ami" "nat_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_security_group" "nat_sg" {
+  name        = "${var.environment}-nat-sg"
+  description = "Security group for NAT instance"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "Allow HTTTP from private subnet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.private_cidr]
+  }
+
+  ingress {
+    description = "Allow HTTPS from private subnet"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [var.private_cidr]
+  }
+
+  egress {
+    description = "Allow HTTP outbound"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow HTTPS outbound"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.environment}-nat-sg"
+    }
+  )
+}
+
+resource "aws_instance" "nat" {
+  ami           = data.aws_ami.nat_ami.id
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.public.id
+
+  vpc_security_group_ids = [aws_security_group.nat_sg.id]
+
+  source_dest_check = false
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.d/custom-ip-forwarding.conf
+              sysctl -p /etc/sysctl.d/custom-ip-forwarding.conf
+
+              /sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+              yum install -y iptables-services
+              service iptables save
+              systemctl enable iptables
+              systemctl start iptables
+              EOF
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.environment}-nat-instance"
+    }
+  )
+}
+
+resource "aws_route" "private_nat_gateway" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  network_interface_id   = aws_instance.nat.primary_network_interface_id
+}
--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -9,3 +9,7 @@ output "security_group_id" {
 output "vpc_id" {
   value = aws_vpc.main.id
 }
+
+output "private_subnet_id" {
+  value = aws_subnet.private.id
+}
