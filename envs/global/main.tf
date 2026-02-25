6fbdb59412974c51204d4c8539c9cfde04fe11e9
Piotr Sacharuk <piotr.sacharuk@gmail.com>
2026-02-25 15:16:06 +0100

    Backend Security Hardening

--- envs/global/main.tf
+++ envs/global/main.tf
@@ -3,3 +3,33 @@ module "billing_alarm" {
   alert_email       = var.alert_email
   billing_threshold = var.billing_threshold
 }
+
+#################################
+# S3 Backend Security Hardening
+#################################
+resource "aws_s3_bucket_public_access_block" "backend" {
+  bucket = "cloud-devops-platform-free-tier"
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "backend" {
+  bucket = "cloud-devops-platform-free-tier"
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "backend" {
+  bucket = "cloud-devops-platform-free-tier"
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
